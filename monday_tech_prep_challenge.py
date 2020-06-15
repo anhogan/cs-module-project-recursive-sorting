4b59b642ee60931135a9ee889f5ed71d8f5f405d
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-15 17:34:59 -0400

    Recursive binary search solution

---
+++ monday_tech_prep_challenge.py
@@ -0,0 +1,10 @@
+# Print each item on a separate line
+# Loop through each element and print it
+
+random_array = ['Joe', 2, 'Ted', 4.98, 14, 'Sam', 'void *', '42', 'float', 'pointers', 5006]
+
+def print_array(arr):
+  for i in arr:
+    print(i)
+
+print_array(random_array)
